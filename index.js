b0c1596406123e8d187ace0df5b513bb1c219cd2
黃柏崴 <Johnny@huangboweide-MacBook-Pro.local>
2016-03-03 18:42:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+/**
+ *
+ * The program would push only one request at one time,
+ * preventing the ptt server to crash or forbide the request from your Address
+ * pttCrawler
+ * Copyright(c) 2016-2020 Bo-Wei Huang National Dong Hwa University
+ * For Academic Use
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/crawler.js');
